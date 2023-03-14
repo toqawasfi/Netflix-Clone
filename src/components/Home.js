19c390ca15b5b46f2ee66fe959cd8c5ee449ef45
toqawasfi <toqawasfi123@gmail.com>
2023-03-14 20:52:45 +0000

    under progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,39 @@
 import { useEffect, useState } from "react";
 import NavBar from './NavBar';
-import MovieList from './MovieList'
+import MovieList from './MovieList';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 // import axios from 'axios';
 
-function Home()
-{
-   
-    return(
+function Home() {
+    const [moviesArr, setmoviesArr] = useState([]);
+    const serverUrl = `http://localhost:3000/trending`;
+    const sendReq = async () => {
+        const response = await fetch(serverUrl);
+        const data = await response.json();
+        console.log(data);
+        setmoviesArr(data);
+    }
+    return (
         <>
-    
-        
-        <NavBar />
-        <MovieList />
+            <p>HEllo from Home</p>
+            <button onClick={sendReq}>send req</button>
+            {moviesArr.map((item) => {
+                return <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={item.poster_path} />
+                            <Card.Body>
+                                <Card.Title>{item.title}</Card.Title>
+                                <Card.Text>
+                                    <p>{item.overview}</p>
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                
+            })}
+           
         </>
+
     )
 }
 export default Home;
